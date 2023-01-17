437a7eb4faeeda2bd163d977311d73a407c2f36b
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2023-01-17 15:42:44 -0500

    Added Num class

---
+++ src/num.py
@@ -0,0 +1,50 @@
+import math
+
+
+class Num:
+	"""
+	Summarizes a stream of numbers.
+	"""
+	def __init__(self):
+		self.n = 0
+		self.mu = 0
+		self.m2 = 0
+
+		self.lo = math.inf
+		self.hi = -math.inf
+
+	def add(self, n: float) -> None:
+		"""
+		Adds n and updates lo, hi and stuff needed for standard deviation.
+
+		:param n: Number to add
+		:return: None
+		"""
+		if n != "?":
+			self.n += 1
+
+			d = n - self.mu
+
+			self.mu += (d / self.n)
+			self.m2 += d * (n - self.mu)
+
+			self.lo = min(n, self.lo)
+			self.hi = max(n, self.hi)
+
+	def mid(self) -> float:
+		"""
+		Returns mean of the numbers added to the stream.
+
+		:return: Mean of the numbers
+		"""
+		return self.mu
+
+	def div(self) -> float:
+		"""
+		Returns standard deviation of the numbers using Welford's algorithm.
+
+		:return: Standard deviation of the numbers
+		"""
+		return 0 if (self.m2 < 0 or self.n < 2) else math.pow((self.m2 / (self.n - 1)), 0.5)
+
+
