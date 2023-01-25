dadc4b1b160586a3816cffa6c1b1f422ec4f5368
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2023-01-24 19:10:33 -0500

    Refactored code and added docstrings

--- src/HW2/cols.py
+++ src/HW2/cols.py
@@ -1,27 +1,38 @@
-from num import Num
-from sym import Sym
 import re
-from utils import csv
 
-# -- ### COLS
-# -- Factory for managing a set of NUMs or SYMs
-class COLS:
-    def __init__(self,t): # --> COLS; generate NUMs and SYMs from column names
+from num import Num
+from row import Row
+from sym import Sym
+
+
+class Cols:
+    """
+    Factory for managing a set of NUMs or SYMs
+    """
+    def __init__(self, t):
         self.names, self.all, self.x, self.y, self.klass = t, [], [], [], None
-        for n,s in enumerate(t):
-            col = Num(n,s)  if re.findall("^[A-Z]+", s) else Sym(n,s)
+
+        for n, s in enumerate(t):
+            # Generate Nums and Syms from column names
+            col = Num(n, s) if re.findall("^[A-Z]+", s) else Sym(n, s)
             self.all.append(col)
+
             if not re.findall("X$", s):
-                if re.findall("!$", s): 
-                    self.klass = col 
-                if re.findall("[!+-]$", s) :
+                if re.findall("!$", s):
+                    self.klass = col
+
+                if re.findall("[!+-]$", s):
                     # re.match("[!+-]$", s) and self.y or self.x, 
                     self.y.append(col)
                 else:
                     self.x.append(col)
 
-    def add(self,row): # nil; update the (not skipped) columns with details from `row`
-        for _,t in enumerate(zip(self.x,self.y)): 
-            for _,col in enumerate(t):
-                col.add(row.cells[col.at])
+    def add(self, row: Row) -> None:
+        """
+        Updates the columns with details from row
 
+        :param row: Row to add
+        """
+        for _, t in enumerate(zip(self.x, self.y)):
+            for _, col in enumerate(t):
+                col.add(row.cells[col.at])
