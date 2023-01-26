be1d927b0e164238bcdd4045cdfdcecdc6806655
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2023-01-26 17:21:50 -0500

    Simplified code and fixed stats()

--- src/HW2/data.py
+++ src/HW2/data.py
@@ -1,32 +1,54 @@
+from typing import Union, List, Dict
+
 from cols import Cols
+from num import Num
 from row import Row
-from utils import kap, map, csv
+from sym import Sym
+from utils import csv
 
 
 class Data:
-    def __init__(self, src) -> None:
+    def __init__(self, src: Union[str, List]) -> None:
         self.rows = list()
         self.cols = None
+
         if type(src) == str:
             csv(src, self.add)
         else:
-            map(src or {}, self.add)
+            map(self.add, src or [])
 
-    def add(self, t):
+    def add(self, t: Union[List, Row]):
+        """
+        Adds a new row and updates column headers.
+
+        :param t: Row to be added
+        """
         if self.cols:
-            t = t.cells and t or Row(t)
+            t = t if isinstance(t, Row) else Row(t)
+
             self.rows.append(t)
             self.cols.add(t)
         else:
             self.cols = Cols(t)
 
-    def clone(self, init):
+    def clone(self, init: List) -> 'Data':
+        """
+        Returns a clone with the same structure as self.
+
+        :param init: Initial data for the clone
+        """
         data = Data(list(self.cols.names))
-        map(init or [], self.add)
+        map(self.add, init or [])
+
         return data
 
-    def stats(self, cols, nplaces, what="mid"):
-        def fun(k, col):
-            return col.rnd((col, what)(), nplaces), col.txt
+    def stats(self, cols: List[Union[Sym, Num]], nplaces: int, what: str = "mid") -> Dict:
+        """
+        Returns mid or div of cols (defaults to i.cols.y).
 
-        return kap(fun, cols or self.cols.y)
+        :param cols: Columns to collect statistics for
+        :param nplaces: Decimal places to round the statistics
+        :param what: Statistics to collect
+        :return: Dict with all statistics for the columns
+        """
+        return dict(sorted({col.txt: col.rnd(getattr(col, what)(), nplaces) for col in cols or self.cols.y}.items()))
