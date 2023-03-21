0ee6168aa7f993d542da8c2e13393781a4995427
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2023-03-20 22:13:19 -0400

    Added check_basic

--- src/HW7/main.py
+++ src/HW7/main.py
@@ -2,6 +2,7 @@ import random
 from options import options
 from num import Num
 from stats import samples, gaussian
+from utils import cliffsDelta
 
 help = """
 
@@ -81,7 +82,27 @@ def check_gauss():
     for i in range(10**4):
         t.append(gaussian(10,2))
     n=Num(t)
-    print("\t",n.n,n.mu,n.sd) 
+    print("\t",n.n,n.mu,n.sd)
+
+
+def check_basic():
+    print(
+        "\t\ttrue",
+        bootstrap({8, 7, 6, 2, 5, 8, 7, 3}, {8, 7, 6, 2, 5, 8, 7, 3}),
+        cliffsDelta({8, 7, 6, 2, 5, 8, 7, 3}, {8, 7, 6, 2, 5, 8, 7, 3})
+    )
+
+    print(
+        "\t\tfalse",
+        bootstrap({8, 7, 6, 2, 5, 8, 7, 3}, {9, 9, 7, 8, 10, 9, 6}),
+        cliffsDelta({8, 7, 6, 2, 5, 8, 7, 3}, {9, 9, 7, 8, 10, 9, 6})
+    )
+
+    print(
+        "\t\tfalse",
+        bootstrap({0.34, 0.49, 0.51, 0.6, .34, .49, .51, .6}, {0.6, 0.7, 0.8, 0.9, .6, .7, .8, .9}),
+        cliffsDelta({0.34, 0.49, 0.51, 0.6, .34, .49, .51, .6}, {0.6, 0.7, 0.8, 0.9, .6, .7, .8, .9})
+    )
 
 
 eg("ok", "check ok", check_ok)
